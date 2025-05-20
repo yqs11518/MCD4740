4f326b9872eca5798f1320aa335405e73dc3a1cd
yqs11518 <37836294+yqs11518@users.noreply.github.com>
2025-05-20 10:08:59 +1000

    Delete A3 directory

--- A3/faq.js
+++
@@ -1,7 +0,0 @@
-const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
-for (const item of faqItems) {
-    const onClick = () => {
-        item.classList.toggle('active')
-    }
-    item.addEventListener('click', onClick)
-}
