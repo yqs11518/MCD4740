468480e9f82cd99ad8fb7705c01dd1751454c562
yqs11518 <37836294+yqs11518@users.noreply.github.com>
2025-05-20 10:07:44 +1000

    Add files via upload

---
+++ A3/faq.js
@@ -0,0 +1,7 @@
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+for (const item of faqItems) {
+    const onClick = () => {
+        item.classList.toggle('active')
+    }
+    item.addEventListener('click', onClick)
+}
